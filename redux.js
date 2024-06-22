91e746de8cb873ffe373965a77149271d1c633a3
Nishant3105 <nishantkumar2522@gmail.com>
2024-06-22 19:24:02 +0530

    .

--- redux.js
+++ redux.js
@@ -1,11 +1,12 @@
 const redux = require('redux')
 
 const counterReducer = (state = { counter: 0 }, action) => {
-    if (action.type === 'increment')
-        return { counter: state.counter + 1 }
-    if (action.type === "decrement") {
-        return { counter: state.counter - 1 }
+    if (action.type === 'incrementby2')
+        return { counter: state.counter + 2 }
+    if (action.type === "decrementby2") {
+        return { counter: state.counter - 2 }
     }
+    return state
 }
 
 const store = redux.createStore(counterReducer)
@@ -19,8 +20,8 @@ const counterSubscriber = () => {
 
 store.subscribe(counterSubscriber)
 
-store.dispatch({ type: 'increment' })
-store.dispatch({ type: 'increment' })
-store.dispatch({ type: 'increment' })
-store.dispatch({ type: 'decrement' })
-store.dispatch({ type: 'increment' })
+store.dispatch({ type: 'incrementby2' })
+store.dispatch({ type: 'incrementby2' })
+store.dispatch({ type: 'incrementby2' })
+store.dispatch({ type: 'decrementby2' })
+store.dispatch({ type: ''})
